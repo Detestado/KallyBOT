cdded6497f33f2e4eb7d74d0da3129dbedd19b3f
LockDzn <ryanssouza00@gmail.com>
2018-03-05 19:24:14 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,36 @@
+const Discord = require('discord.js');
+const colors = require('colors');
+const client = new Discord.Client();
+const preferencias = require('./config.json');
+const prefix = preferencias.prefix;
+const fs = require('fs');
+var comandos = new Discord.Collection();
+
+client.login(process.env.BOT_TOKEN);
+
+client.on('ready', () =>{
+
+
+console.log('O Bot foi iniciado com sucesso.'.green);
+console.log(`O meu prefixo é ${prefix}`);
+});
+
+
+client.on('message', message =>{
+
+var autor = message.author;
+var msg = message.content.toUpperCase();
+var cont = message.content.slice(prefix.lenght).split('');
+
+if(!message.content.startsWith(prefix)) return;
+
+  const args = message.content.slice(prefix.length).trim().split(/ +/g);
+  const command = args.shift().toLowerCase();
+
+  try {
+    let commandFile = require(`./comandos/${command}.js`);
+    commandFile.run(client, message, args);
+  } catch (err) {
+    console.error(err);
+  }
+});
