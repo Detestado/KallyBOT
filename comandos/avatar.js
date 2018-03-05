e87a9852b51aca17d6346a1db2b01937c75eb902
LockDzn <ryanssouza00@gmail.com>
2018-03-05 20:44:58 -0300

    Update avatar.js

--- comandos/avatar.js
+++ comandos/avatar.js
@@ -3,13 +3,11 @@ const Discord = require("discord.js");
 module.exports.run = async (client, message, args) =>{
 
    message.delete().catch(O_o=>{});
-   const embed = new Discord.RichEmbed()
-    .setColor(0x00AE86)
-    .setTimestamp()
-    .addField('Action:', 'Un/Mute')
-    .addField('User:', `AAAAA`)
-    .addField('Modrator:', `${message.author.username}#${message.author.discriminator}`);
+   const avatar = new Discord.RichEmbed()
+      .setImage(message.author.avatarURL)
+      .setColor("#00a4ce")
+      .setFooter("© Kallyᴮᴱᵀᴬ")
    
-   message.channel.send(embed)
+   message.channel.send(avatar)
            
 }
