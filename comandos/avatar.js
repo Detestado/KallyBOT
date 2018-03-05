8dca311e1729ecc4608909db570502d2ef411093
LockDzn <ryanssouza00@gmail.com>
2018-03-05 20:37:51 -0300

    Update avatar.js

--- comandos/avatar.js
+++ comandos/avatar.js
@@ -10,6 +10,6 @@ module.exports.run = async (client, message, args) =>{
     .addField('User:', `AAAAA`)
     .addField('Modrator:', `${message.author.username}#${message.author.discriminator}`);
    
-   message.channel.sendEmbed(embed)
+   message.channel.send(embed)
            
 }
