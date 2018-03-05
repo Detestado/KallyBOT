387616cb42fb6ade822f617953d67bf7197fa42b
LockDzn <ryanssouza00@gmail.com>
2018-03-05 20:59:08 -0300

    Update avatar.js

--- comandos/avatar.js
+++ comandos/avatar.js
@@ -5,7 +5,7 @@ module.exports.run = async (client, message, args) =>{
    message.delete().catch(O_o=>{});
    const avatar = new Discord.RichEmbed()
       .setDescription(`**Clique [aqui](message.author.avatarURL) para baixar a imagem!**`)
-      .setAuthor(`:frame_photo: ${message.user.name}`)
+      .setAuthor(`:frame_photo: ${message.author.username}`)
       .setImage(message.author.avatarURL)
       .setColor("#00a4ce")
       .setFooter("© Kallyᴮᴱᵀᴬ")
