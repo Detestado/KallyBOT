b8984893f0d081862b0bebe52c32ce09d77a7dd2
LockDzn <ryanssouza00@gmail.com>
2018-03-05 19:42:22 -0300

    Create avatar.js

---
+++ comandos/avatar.js
@@ -0,0 +1,11 @@
+module.exports.run = async (client, message, args) =>{
+
+   message.delete().catch(O_o=>{});
+   const avatar = new Discord.RichEmbed()
+      .setImage(message.author.avatarURL)
+      .setColor("#00a4ce")
+      .setFooter("© Kallyᴮᴱᵀᴬ")
+   
+   message.channel.send(avatar)
+           
+}
