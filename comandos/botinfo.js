6f858d4b68f7a3acf2dad686285c1f8f4d0221de
LockDzn <ryanssouza00@gmail.com>
2018-03-06 15:03:28 -0300

    Update botinfo.js

--- comandos/botinfo.js
+++ comandos/botinfo.js
@@ -6,15 +6,17 @@ module.exports.run = async (client, message, args) =>{
    message.delete().catch(O_o=>{});
    const info = new Discord.RichEmbed()
       .setColor("#0092ca")
-      .setAuthor(message.author.username, message.author.avatarURL)
-      .setThumbnail(message.author.avatarURL)
+      .setAuthor(client.user.username, client.user.avatarURL)
+      .setThumbnail(client.user.avatarURL)
 
       .setDescription(`Olá, me chamo Kally, sou focado na moderação dos servidores do discord,
-      entre no meu servidor [aqui](https://discord.gg/nbEqSrv), 
-      lá o meu dono pode te ajuda e você vai receber atualizações!\n \n
-      Estou em ${client.guilds.size} servidores no discord, sou feito em **JS** (**Node.js**/**JavaScript**), Entre no meu **site** :) (**SENDO FEITO**)
-      \nUse **!ajuda** para saber meus comandos!`)
-
+entre no meu servidor [aqui](https://discord.gg/nbEqSrv), 
+lá o meu dono pode te ajuda e você vai receber atualizações!\n \n
+Estou em ${client.guilds.size} servidores no discord, sou feito em **JS** (**Node.js**/**JavaScript**), Entre no meu **site** :) (**SENDO FEITO**)
+\nUse **!ajuda** para saber meus comandos!`)
+   
+      .addField("Melhores pessoas:", "``LockDzn#8368`` Foi ele que me criou! :grin: ")
+      .setFooter("© Kallyᴮᴱᵀᴬ | Site: bit.ly/LockDzn",
    
    message.channel.send(info)
    
