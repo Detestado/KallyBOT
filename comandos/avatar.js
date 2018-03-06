0e46c6ea95deaf477a34cdd3d7d1707617abc14a
LockDzn <ryanssouza00@gmail.com>
2018-03-06 00:09:48 -0300

    📥 | Novo comando !avatar

--- comandos/avatar.js
+++ comandos/avatar.js
@@ -3,6 +3,21 @@ const Discord = require("discord.js");
 module.exports.run = async (client, message, args) =>{
 
    message.delete().catch(O_o=>{});
+   let member = message.mentions.members.first();
+   
+   if(!member) {
+     
+   const avatar = new Discord.RichEmbed()
+      .setDescription(`**Clique [aqui](${member.user.avatarURL}) para baixar a imagem!**`)
+      .setAuthor(`Foto de avatar de ${member.user.username}`)
+      .setImage(member.user.avatarURL)
+      .setColor("#00a4ce")
+      .setFooter("© Kallyᴮᴱᵀᴬ")
+   
+      message.channel.send(avatar)
+   
+   } else {
+      
    const avatar = new Discord.RichEmbed()
       .setDescription(`**Clique [aqui](${message.author.avatarURL}) para baixar a imagem!**`)
       .setAuthor(`Foto de avatar de ${message.author.username}`)
@@ -10,6 +25,7 @@ module.exports.run = async (client, message, args) =>{
       .setColor("#00a4ce")
       .setFooter("© Kallyᴮᴱᵀᴬ")
    
-   message.channel.send(avatar)
-           
+      message.channel.send(avatar)
+      
+   }
 }
