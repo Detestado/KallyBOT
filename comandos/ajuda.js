f824a3ca52ffac8ab6ecb49ec8fd6a7df6dd6c42
LockDzn <ryanssouza00@gmail.com>
2018-03-06 17:37:47 -0300

    📥 | Novo comando !ajuda

--- comandos/ajuda.js
+++ comandos/ajuda.js
@@ -3,8 +3,6 @@ const Discord = require("discord.js");
 module.exports.run = async (client, message, args) =>{
 
    const question = '❓';
-   const cop = message.emojis.get("name", "cop");
-   const card_box = message.emojis.get("name", "card_box");
    
    
    message.delete().catch(O_o=>{});
@@ -16,7 +14,7 @@ module.exports.run = async (client, message, args) =>{
       .setFooter("© Kallyᴮᴱᵀᴬ - bit.ly/LockDzn")
 
    const ajuda2 = new Discord.RichEmbed()
-      .setAuthor(`${cop} Moderação`)
+      .setAuthor(` Moderação`)
       .setDescription(`*Comandos que vão ajudar a administrar servidores!* 
 \n**!mute** @usuário motivo - Ira mutar o usuário mencionado.
 **!unmute** @usuário - Ira desmutar o usuário mencionado.
@@ -26,7 +24,7 @@ module.exports.run = async (client, message, args) =>{
       .setFooter("© Kallyᴮᴱᵀᴬ Moderação")
       
    const ajuda3 = new Discord.RichEmbed()
-      .setAuthor(`${card_box} Outros`)
+      .setAuthor(` Outros`)
       .setDescription(`*Comandos que "aleatorios" que podem te ajudar ou não. Comandos que não se encaixam nas outras categorias!* \n
 \n**!ajuda** - Ira te mandar a lista de comandos.
 **!botinfo** - Ira mostrar as informaçoes de min. \n**!avatar** @usuário - Ira mostrar o avatar de um usuário.`)
