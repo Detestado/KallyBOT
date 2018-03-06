bbb602450a9a84ef644b4d05c453a4f4251cebf6
LockDzn <ryanssouza00@gmail.com>
2018-03-06 14:54:19 -0300

    Update botinfo.js

--- comandos/botinfo.js
+++ comandos/botinfo.js
@@ -7,21 +7,21 @@ module.exports.run = async (client, message, args) =>{
    message.delete().catch(O_o=>{});
    const info = new Discord.RichEmbed()
    
-   .setColor("#0092ca")
-   .setAuthor(message.author.username, message.author.avatarURL)
-   .setThumbnail(message.author.avatarURL)
-   
-   .setDescription(`Olá, me chamo Kally, sou focado na moderação dos servidores do discord,
-   entre no meu servidor [aqui](https://discord.gg/nbEqSrv), 
-   lá o meu dono pode te ajuda e você vai receber atualizações!\n \n
-   Estou em ${client.guilds.size} servidores no discord, sou feito em **JS** (**Node.js**/**JavaScript**), Entre no meu **site** :) (**SENDO FEITO**)
-   \nUse **!ajuda** para saber meus comandos!`)
-   
-   .addField("Obrigado a:",
-    ```LockDzn#8368`` Foi ele que fez! :)\n
-    ``${message.author.username}#${message.author.discriminator}`` Por estar falando comigo! :smile:`)
-  
-   .setFooter("© Kallyᴮᴱᵀᴬ | bit.ly/LockDzn")
+      .setColor("#0092ca")
+      .setAuthor(message.author.username, message.author.avatarURL)
+      .setThumbnail(message.author.avatarURL)
+
+      .setDescription(`Olá, me chamo Kally, sou focado na moderação dos servidores do discord,
+      entre no meu servidor [aqui](https://discord.gg/nbEqSrv), 
+      lá o meu dono pode te ajuda e você vai receber atualizações!\n \n
+      Estou em ${client.guilds.size} servidores no discord, sou feito em **JS** (**Node.js**/**JavaScript**), Entre no meu **site** :) (**SENDO FEITO**)
+      \nUse **!ajuda** para saber meus comandos!`)
+
+      .addField("Obrigado a:",
+       ```LockDzn#8368`` Foi ele que fez! :)\n
+       ``${message.author.username}#${message.author.discriminator}`` Por estar falando comigo! :smile:`)
+
+      .setFooter("© Kallyᴮᴱᵀᴬ | bit.ly/LockDzn")
    
    message.channel.send(info)
    
