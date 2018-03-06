bd1880720f8f75825a30a299aa510b15c51c88c0
LockDzn <ryanssouza00@gmail.com>
2018-03-06 17:05:46 -0300

    Update app.js

--- app.js
+++ app.js
@@ -33,4 +33,7 @@ if(!message.content.startsWith(prefix)) return;
   try {
     let commandFile = require(`./comandos/${command}.js`);
     commandFile.run(client, message, args);
+   } catch (err) {
+    console.error(err);
+   }
 });
