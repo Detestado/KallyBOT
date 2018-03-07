1c70396ffdbc8f0862c12d95a29a8a8cc04b9360
LockDzn <ryanssouza00@gmail.com>
2018-03-07 15:47:41 -0300

    📥 | Novo comando !teste

--- comandos/teste.js
+++ comandos/teste.js
@@ -5,10 +5,9 @@ module.exports.run = async (client, message, args) =>{
    message.delete().catch(O_o=>{});
    if (message.author.id === '244537374258888725') {
      const teste = new Discord.RichEmbed()
-        .setDescription("Teste :heart: ")
-        .setAuthor(`Teste :heart:`)
+        .setDescription("Test :heart: ")
+        .setAuthor(`Test :heart:`)
         .setColor("#00a4ce")
-        .setFooter("© Kallyᴮᴱᵀᴬ")
    
     message.channel.send(teste)
     
