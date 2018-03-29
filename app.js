a149731e6e6df1a0262e7a5a4b88b674387f1d75
LockDzn <ryanssouza00@gmail.com>
2018-03-28 21:07:45 -0300

    Update app.js

--- app.js
+++ app.js
@@ -9,10 +9,10 @@ var comandos = new Discord.Collection();
 client.login(process.env.BOT_TOKEN);
 
 
-fs.readdir("./eventos/", (err, files) => {
+fs.readdir("/eventos/", (err, files) => {
     if (err) return console.error("ERRO: " + err);
     files.forEach(file => {
-        let eventFunction = require(`./eventos/${file}`);
+        let eventFunction = require(`/eventos/${file}`);
         let eventName = file.split(".")[0];
         client.on(eventName, (...args) => eventFunction.run(client, ...args));
     });
