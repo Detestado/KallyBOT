7cdb99c29ab1fd424cd62259f3fa69bfc43c1809
LockDzn <ryanssouza00@gmail.com>
2018-03-31 01:34:23 -0300

    📥 | Novo comando !webnamorada

--- comandos/webnamorada.js
+++ comandos/webnamorada.js
@@ -8,19 +8,17 @@ module.exports.run = async (client, message, args) =>{
    if(message.channel.id = "429489240808751115"){
      const webn = new Discord.WebhookClient('429490324105199616', 'tjjIxpzILLTQwXfA1wUwBfwNMr8VEuGSS1I-DFzmeA-GSXiRryvoB-JjixU5KwjhAGND');
      
-     if(msg = "Oi amor")
+     if(msg = "Oi amor"){
         if(message.author.id = "244537374258888725"){
            webn.send('<@244537374258888725> Oi amorzinho! <3');
         } else {
            webn.send(`${message.author} ??? quem é você??`);
-     } 
-  } // FIM
-     if(msg = "é você na foto?")
+   }}}// FIM
+     if(msg = "é você na foto?"){
         if(message.author.id = "244537374258888725"){
            webn.send(`${message.author} Sim, sou eu.`);
         } else {
            webn.send(`${message.author} não, essa foto foi para inganar o Lock.`);
-     } 
-  } // FIM
+   }}} // FIM
 
 }
