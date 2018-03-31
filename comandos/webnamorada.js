24d1a1aa4c887d3fd08200e01ac6c0ce3adc44ca
LockDzn <ryanssouza00@gmail.com>
2018-03-31 01:13:07 -0300

    📥 | Novo comando !webnamorada

---
+++ comandos/webnamorada.js
@@ -0,0 +1,19 @@
+const Discord = require("discord.js");
+
+module.exports.run = async (client, message, args) =>{
+
+   message.delete().catch(O_o=>{});
+   let msg = args.slice(1).join(' ');
+   
+   if(message.channel.id = "429489240808751115")
+     const webn = new Discord.WebhookClient('429490324105199616', 'tjjIxpzILLTQwXfA1wUwBfwNMr8VEuGSS1I-DFzmeA-GSXiRryvoB-JjixU5KwjhAGND');
+     
+     if(msg = "Oi amor"){
+        webn.send('@LockDzn#8368 Oi amorzinho! <3');
+     } else {
+        webn.send(`${message.author} ??? quem é você??`);
+     
+     }
+    
+  }
+}
