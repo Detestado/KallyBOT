f5ade89875204a83e5c3208073995041133979e6
LockDzn <ryanssouza00@gmail.com>
2018-04-01 18:33:46 -0300

    🔄 | Comando !ajuda atualizado

--- comandos/ajuda.js
+++ comandos/ajuda.js
@@ -15,10 +15,10 @@ module.exports.run = async (client, message, args) =>{
    const ajuda2 = new Discord.RichEmbed()
       .setAuthor(`Moderação`)
       .setDescription(`*Comandos que vão ajudar a administrar servidores!* 
-\n**!mute** @usuário motivo - Ira mutar o usuário mencionado.
-**!unmute** @usuário - Ira desmutar o usuário mencionado.
-**!ban** @usuário motivo - Ira banir o usuário mencionado do seu servidor do discord.
-**!anuncio** mensagem do anuncio - Ira mandar um anuncio no chat que o comandos foi executado.`)
+\n**k!mute** @usuário motivo - Ira mutar o usuário mencionado.
+**k!unmute** @usuário - Ira desmutar o usuário mencionado.
+**k!ban** @usuário motivo - Ira banir o usuário mencionado do seu servidor do discord.
+**k!anuncio** mensagem do anuncio - Ira mandar um anuncio no chat que o comandos foi executado.`)
       .setThumbnail("https://i.imgur.com/ZmWM1UT.png")
       .setColor("#ff0000")
       .setFooter("© Kallyᴮᴱᵀᴬ Moderação")
@@ -26,8 +26,8 @@ module.exports.run = async (client, message, args) =>{
    const ajuda3 = new Discord.RichEmbed()
       .setAuthor(`Outros`)
       .setDescription(`*Comandos que são "aleatorios" que podem te ajudar ou não. Comandos que não se encaixam nas outras categorias!* \n
-\n**!ajuda** - Ira te mandar a lista de comandos.
-**!botinfo** - Ira mostrar as informaçoes de min. \n**!avatar** @usuário - Ira mostrar o avatar de um usuário.`)
+\n**k!ajuda** - Ira te mandar a lista de comandos.
+**k!botinfo** - Ira mostrar as informaçoes de min. \n**!avatar** @usuário - Ira mostrar o avatar de um usuário.`)
       .setThumbnail("https://i.imgur.com/lEI7Gm6.gif")
       .setColor("#00f782")
       .setFooter("© Kallyᴮᴱᵀᴬ Outros")
