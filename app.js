6c7065623d50531b9f7f367c0680d6ac004b802d
LockDzn <ryanssouza00@gmail.com>
2018-04-01 14:15:05 -0300

    Update app.js

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ client.on("guildCreate", guild => {
       .setAuthor(`${guild.name} | Entrei`)
       .setDescription(`Entrei no servidor: ${guild.name} (id: ${guild.id})`)
       .addField("Membros", `Esse servidor tem ${guild.memberCount} membros`)
-      .addField("Dono", `${guild.member.owner}`)
+      .addField("Dono", `A`)
       .setColor("00e7ff")
   
   
