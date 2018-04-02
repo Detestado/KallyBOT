5844fe874eefb9d647217601b46561821d5204f5
LockDzn <ryanssouza00@gmail.com>
2018-04-02 13:56:20 -0300

    Update guildMemberAdd.js

--- eventos/guildMemberAdd.js
+++ eventos/guildMemberAdd.js
@@ -1,20 +1,8 @@
 const moment = require("moment");
 
 exports.run = (client, member) => {
-  if(guild.id = "420316735149965322"){
-    const { schemaManager } = client.settingGateway;
-    if (!schemaManager.schema["rawLogs"]) {
-      schemaManager.add("rawLogs", {type: "String", array: true, default: []}, true);
-      schemaManager.add("rawLogChannel", {type: "String"});
-    }
 
-    if(!member || !member.id || !member.guild) return;
-    const conf = member.guild.settings;
-    if(!conf.rawLogs.includes("all") && !conf.rawLogs.includes("messageUpdate") || !conf.rawLogChannel) return;
-    const channel = member.guild.channels.get(conf.rawLogChannel);
-    if(!channel) return;
-    const fromNow = moment(member.user.createdTimestamp).fromNow();
-    const isNew = (new Date() - member.user.createdTimestamp) < 900000 ? "🆕" : "";
-    channel.send(`📥 ${member.user.tag} (${member.user.id}) joined. Created: ${fromNow} ${isNew}`); 
-  }
+  const channel = member.guild.channels.get("430142996051263490");
+
+  channel.send(`📥 ${member.user.tag} (${member.user.id}) joined.`);
 };
