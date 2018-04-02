7b0b0a1f79a1deff623d922f534d059eae8372e3
LockDzn <ryanssouza00@gmail.com>
2018-04-02 12:59:03 -0300

    Update guildMemberAdd.js

--- eventos/guildMemberAdd.js
+++ eventos/guildMemberAdd.js
@@ -1,6 +1,9 @@
-exports.run = (client) => {    
+const Discord = require("discord.js");
+const client = new Discord.Client();
 
-// INICIO KALLY SERV
+exports.run = (client) => {    
+  
+  // INICIO KALLY SERV
   if(guild.id = "420316735149965322"){
     member.guilds.find("id", "420316735149965322").channels.get("430142996051263490").send(`"${member.user.username}" has joined this server`);
     
