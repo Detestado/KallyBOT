1fd7b0b48dbdc600653260a2341d06212e103927
LockDzn <ryanssouza00@gmail.com>
2018-04-02 13:14:01 -0300

    Update app.js

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ client.on("guildMemberAdd", (guild, member) => {
 
     // INICIO KALLY SERV
    if(guild.id = "420316735149965322"){
-     member.guilds.find("id", "420316735149965322").channels.get("430142996051263490").send(`"${member.user.username}" has joined this server`);
+     member.guild.channels.get("430142996051263490").send(`"${member.user.username}" has joined this server`);
      
    } // FIM KALLY SERV
 });
