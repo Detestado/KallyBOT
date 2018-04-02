1bd5996e8619b7132b1bf6bacc96e43776c3a024
LockDzn <ryanssouza00@gmail.com>
2018-04-02 12:44:52 -0300

    Update novo-membro.js

--- eventos/novo-membro.js
+++ eventos/novo-membro.js
@@ -1,9 +1,5 @@
-const moment = require("moment");
-
-exports.run = (client, member) => {
     // INICIO KALLY SERV
     if(guild.id = "420316735149965322"){
       member.guilds.find("id", "420316735149965322").channels.get("430142996051263490").send(`"${member.user.username}" has joined this server`);
     
     } // FIM KALLY SERV
-}
