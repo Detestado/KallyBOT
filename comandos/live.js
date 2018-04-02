d7ab60b83c99e092c9378d42e683b5be845ee506
LockDzn <ryanssouza00@gmail.com>
2018-04-02 16:16:16 -0300

    🔄 | Comando k!live atualizado

--- comandos/live.js
+++ comandos/live.js
@@ -2,24 +2,19 @@ const Discord = require("discord.js");
 
 module.exports.run = async (client, message, args) =>{
 
-   if(!message.author.id = "244537374258888725"){
-      let mensg = args.join(" ");
-      if(!mensg)
-      return message.reply("Por favor, digite o que o bot vai transmitir!");
+   if(!message.author.id = "244537374258888725")
+      return message.reply("você não tem permissão! :x:")
+      
+    let mensg = args.join(" ");
+    if(!mensg)
+       return message.reply("Por favor, digite o que o bot vai transmitir!");
       
       
-      message.channel.send(`Agora o Kally esta transmitindo: ${mensg}`)
+    message.channel.send(`Agora o Kally esta transmitindo: ${mensg}`)
 
       
-      client.user.setPresence({ game: { name: mensg, url: 'https://www.twitch.tv/LockDzn', type: 1 } });
-      console.log(`Agora o bot esta transmitindo: ${mensg}`);
+    client.user.setPresence({ game: { name: mensg, url: 'https://www.twitch.tv/LockDzn', type: 1 } });
+    console.log(`Agora o bot esta transmitindo: ${mensg}`);
    
-   
-   } else {
-      message.reply("você não tem permissão! :x:")
-   
-   }
- 
- 
-   
+
 }
