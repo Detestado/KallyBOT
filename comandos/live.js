9e1090d94370f34c2d06a4d65dd4261575ebdea3
LockDzn <ryanssouza00@gmail.com>
2018-04-02 16:19:54 -0300

    🔄 | Comando k!live atualizado

--- comandos/live.js
+++ comandos/live.js
@@ -2,7 +2,7 @@ const Discord = require("discord.js");
 
 module.exports.run = async (client, message, args) =>{
 
-   if(!message.author.id = "244537374258888725")
+   if(!message.author.id != "244537374258888725")
       return message.reply("você não tem permissão! :x:")
       
     let mensg = args.join(" ");
