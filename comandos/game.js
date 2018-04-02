e7059083b68ad2ad2880c30614b0f392388c79bc
LockDzn <ryanssouza00@gmail.com>
2018-04-02 15:39:50 -0300

    📥 | Novo comando !game

---
+++ comandos/game.js
@@ -0,0 +1,22 @@
+const Discord = require("discord.js");
+
+module.exports.run = async (client, message, args) =>{
+
+   if(message.author.id = "244537374258888725"){
+      let mensg = args.join(" ");
+      if(!mensg)
+      return message.reply("Por favor, digite o jogo que o bot vai jogar!");
+      
+      client.user.setPresence({ game: { name: `${mensg}`, type: 0 } });
+      console.log(`Agora o bot esta jogando: ${mensg}`);
+   
+   
+   } else {
+      message.reply("você não tem permissão! :x:")
+   
+   }
+ 
+   
+   
+   
+}
