efe70463c1f981f1c337e225c8565461698070b2
LockDzn <ryanssouza00@gmail.com>
2018-04-03 19:24:10 -0300

    :arrows_counterclockwise: | Comando k!uptime atualizado

--- comandos/uptime.js
+++ comandos/uptime.js
@@ -2,27 +2,13 @@ const Discord = require("discord.js");
 
 module.exports.run = async (client, message, args) =>{
 
-   let member = message.mentions.members.first();
-   
-   if(member) {
      
    const uptime = new Discord.RichEmbed()
-      .setDescription(`${member.user.username} esta online a ${member.user.uptime}`)
-      .setAuthor(`:stopwatch: ${member.user.username} tempo online`)
-      .setImage(member.user.avatarURL)
+      .setDescription(`${client.user.username} esta online a ${client.uptime}`)
+      .setAuthor(`:stopwatch: ${client.user.name} tempo online`)
+      .setThumbnail(client.user.avatarURL)
       .setFooter("© Kallyᴮᴱᵀᴬ")
    
-      message.channel.send(uptime)
+    message.channel.send(uptime)
    
-   } else {
-      
-   const uptime = new Discord.RichEmbed()
-      .setDescription(`${message.author.username} esta online a ${message.author.uptime}`)
-      .setAuthor(`:stopwatch: ${message.author.username} tempo online`)
-      .setImage(message.author.avatarURL)
-      .setFooter("© Kallyᴮᴱᵀᴬ")
-   
-      message.channel.send(uptime)
-      
-   }
 }
