75f98f3340044ddb03fbab01a1356f36ac0a1d75
LockDzn <ryanssouza00@gmail.com>
2018-04-04 00:22:02 -0300

    :arrows_counterclockwise: | Comando k!botinfo atualizado

--- comandos/botinfo.js
+++ comandos/botinfo.js
@@ -15,11 +15,11 @@ module.exports.run = async (client, message, args) =>{
 
       .setDescription(`Olá, me chamo Kally, sou focado na moderação dos servidores do discord, entre no meu servidor [aqui](https://discord.gg/nbEqSrv), 
 lá o meu dono pode te ajuda e você vai receber atualizações e também entre no meu [site](https://kally.glitch.me/)!\n
-Estou em ${client.guilds.size} servidores do discord e estou acordado a **`)
-      .setDescription(uptime += date.getUTCDate() - 1 + `**d, **`) 
-      .setDescription(uptime += date.getUTCHours() + `**h, **`)
-      .setDescription(uptime += date.getUTCMinutes() + `**m, **`)
-      .setDescription(uptime += date.getUTCSeconds() + `**s. Sou feito em **JS** (Node.js/JavaScript) :) 
+Estou em ${client.guilds.size} servidores do discord. \nEstou acordado a`)
+      .setDescription("**" + uptime += date.getUTCDate() - 1 + `**d, `) 
+      .setDescription("**" + uptime += date.getUTCHours() + `**h, `)
+      .setDescription("**" + uptime += date.getUTCMinutes() + `**m,`)
+      .setDescription("**" + uptime += date.getUTCSeconds() + `**s. Sou feito em **JS** (Node.js/JavaScript) :) 
 \nUse **k!ajuda** para saber meus comandos!`)
    
       .addField(":heart: Melhores pessoas:", ` **LockDzn#8368** Foi ele que me criou! :grin:\n**${message.author.username}#${message.author.discriminator}** Por estar falando comigo! :smile: `)
