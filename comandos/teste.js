2805a7992956d119b369e7ea52affc9c0b064280
LockDzn <ryanssouza00@gmail.com>
2018-04-04 18:01:38 -0300

    Update teste.js

--- comandos/teste.js
+++ comandos/teste.js
@@ -3,14 +3,9 @@ const Discord = require("discord.js");
 module.exports.run = async (client, message, args) =>{
 
    message.delete().catch(O_o=>{});
-   if (message.author.id === '244537374258888725') {]
+   if (message.author.id === '244537374258888725') {
      const ayy = client.emojis.find("name", "piingblob");
-     const teste = new Discord.RichEmbed()
-        .setDescription(`Test ${ayy}`)
-        .setAuthor(`Test`, message.guild.iconURL)
-        .setColor("#00a4ce")
-   
-    message.channel.send(teste)
+     message.reply(`${ayy} LMAO`);
     
     
   }
