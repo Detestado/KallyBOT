fa850c36d88f8f2fbea466f6e907e1990a83f030
LockDzn <ryanssouza00@gmail.com>
2018-04-03 23:39:31 -0300

    :arrows_counterclockwise: | Comando k!ajuda atualizado

--- comandos/botinfo.js
+++ comandos/botinfo.js
@@ -5,6 +5,10 @@ module.exports.run = async (client, message, args) =>{
    
    const dono = message.guild.members.get("244537374258888725");
    
+   const dia = uptime += date.getUTCDate() - 1
+   const hora = uptime += date.getUTCHours()
+   const minuto = uptime += date.getUTCMinutes()
+   const segundo = uptime += date.getUTCSeconds()
 
    const info = new Discord.RichEmbed()
       .setColor("#0092ca")
@@ -13,7 +17,7 @@ module.exports.run = async (client, message, args) =>{
 
       .setDescription(`Olá, me chamo Kally, sou focado na moderação dos servidores do discord, entre no meu servidor [aqui](https://discord.gg/nbEqSrv), 
 lá o meu dono pode te ajuda e você vai receber atualizações e também entre no meu [site](https://kally.glitch.me/)!\n
-Estou em ${client.guilds.size} servidores do discord, sou feito em **JS** (**Node.js**/**JavaScript**) :)
+Estou em ${client.guilds.size} servidores do discord e estou acordado a **${dia}**d, **${hora}**h, **${minuto}**m, **${segundo}**s. Sou feito em **JS** (Node.js/JavaScript) :)
 \nUse **k!ajuda** para saber meus comandos!`)
    
       .addField(":heart: Melhores pessoas:", ` **LockDzn#8368** Foi ele que me criou! :grin:\n**${message.author.username}#${message.author.discriminator}** Por estar falando comigo! :smile: `)
