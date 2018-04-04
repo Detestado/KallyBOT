b5e262d71c742646cbda07816ae7c5a0b183f5dd
LockDzn <ryanssouza00@gmail.com>
2018-04-04 17:19:07 -0300

    Update uptime.js

--- comandos/uptime.js
+++ comandos/uptime.js
@@ -4,12 +4,16 @@ module.exports.run = async (client, message, args) =>{
 
     var date = new Date(client.uptime);
     var uptime = '';
+    
+    const d = uptime += date.getUTCDate() - 1 + 'd, ';
+    const h = uptime += date.getUTCHours() + 'h, ';
+    const m = uptime += date.getUTCMinutes() + 'm, ';
+    const s = uptime += date.getUTCSeconds() + 's.';
 
     
     const embed2 = new Discord.RichEmbed()
        .setTitle("🕐 Tempo que eu acordei:")
-       .setDescription('**' + uptime += date.getUTCDate() - 1 + 'd, ' + uptime += date.getUTCHours() + 'h, ')
-       .setDescription(uptime += date.getUTCMinutes() + 'm, ' + uptime += date.getUTCSeconds() + 's.**')
+       .setDescription(s)
     
     
     
