e48c2d42fb290bca7397c80046f6d6e3e684c4d7
LockDzn <ryanssouza00@gmail.com>
2018-04-04 16:42:52 -0300

    Update uptime.js

--- comandos/uptime.js
+++ comandos/uptime.js
@@ -5,10 +5,10 @@ module.exports.run = async (client, message, args) =>{
     var date = new Date(client.uptime);
     var uptime = '';
     
-    const d = date.getUTCDate()
-    const h = date.getUTCHours()
-    const m = date.getUTCMinutes()
-    const s = date.getUTCSeconds()
+    const d = uptime += date.getUTCDate()
+    const h = uptime += date.getUTCHours()
+    const m = uptime += date.getUTCMinutes()
+    const s = uptime += date.getUTCSeconds()
     
     const embed2 = new Discord.RichEmbed()
        .setTitle("🕐 Tempo que eu acordei:")
@@ -17,7 +17,7 @@ module.exports.run = async (client, message, args) =>{
        .setDescription(uptime += date.getUTCMinutes() + ' minuto(s), ')
        .setDescription(uptime += date.getUTCSeconds() + ' segundo(s)')
     
-       .addField("🕐 Estou acordado faz", uptime += d + 'd, ' + uptime += h + 'h, ' + uptime += m + 'm, ' + uptime += s + 's.')
+       .addField("🕐 Estou acordado faz", s)
     
     
     message.channel.send(embed2).then(msg => { msg.react("🕐") });
