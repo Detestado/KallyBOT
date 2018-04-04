a3a77c011863153431028fc83794584a0ccff2d2
LockDzn <ryanssouza00@gmail.com>
2018-04-03 21:16:24 -0300

    :arrows_counterclockwise: | Comando k!avatar atualizado

--- comandos/avatar.js
+++ comandos/avatar.js
@@ -8,7 +8,7 @@ module.exports.run = async (client, message, args) =>{
      
    const avatar = new Discord.RichEmbed()
       .setDescription(`**Clique [aqui](${member.user.avatarURL}) para baixar a imagem!**`)
-      .setAuthor(`Foto de avatar de ${member.user.username}`)
+      .setAuthor(":frame_photo: " + `Foto de avatar de ${member.user.username}`)
       .setImage(member.user.avatarURL)
       .setColor("#00a4ce")
       .setFooter("© Kallyᴮᴱᵀᴬ")
