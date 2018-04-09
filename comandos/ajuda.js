f964f841e6897e9ae3787655998f1587f950dfd9
LockDzn <ryanssouza00@gmail.com>
2018-04-09 17:37:05 -0300

    :arrows_counterclockwise: | Comando k!ajuda atualizado

--- comandos/ajuda.js
+++ comandos/ajuda.js
@@ -1,4 +1,6 @@
 const Discord = require("discord.js");
+const preferencias = require('./../config.json');
+const prefix = preferencias.prefix;
 
 module.exports.run = async (client, message, args) =>{
 
@@ -14,10 +16,10 @@ module.exports.run = async (client, message, args) =>{
    const ajuda2 = new Discord.RichEmbed()
       .setAuthor(`Moderação`)
       .setDescription(`*Comandos que vão ajudar a administrar servidores!* 
-\n**k!mute** @usuário motivo - Ira mutar o usuário mencionado.
-**k!unmute** @usuário - Ira desmutar o usuário mencionado.
-**k!ban** @usuário motivo - Ira banir o usuário mencionado do seu servidor do discord.
-**k!anuncio** mensagem do anuncio - Ira mandar um anuncio no chat que o comandos foi executado.`)
+\n**${prefix}mute** @usuário motivo - Ira mutar o usuário mencionado.
+**${prefix}unmute** @usuário - Ira desmutar o usuário mencionado.
+**${prefix}ban** @usuário motivo - Ira banir o usuário mencionado do seu servidor do discord.
+**${prefix}anuncio** mensagem do anuncio - Ira mandar um anuncio no chat que o comandos foi executado.`)
       .setThumbnail("https://i.imgur.com/ZmWM1UT.png")
       .setColor("#ff0000")
       .setFooter("© Kallyᴮᴱᵀᴬ Moderação")
@@ -25,8 +27,10 @@ module.exports.run = async (client, message, args) =>{
    const ajuda3 = new Discord.RichEmbed()
       .setAuthor(`Outros`)
       .setDescription(`*Comandos que são "aleatorios" que podem te ajudar ou não. Comandos que não se encaixam nas outras categorias!* \n
-\n**k!ajuda** - Ira te mandar a lista de comandos.
-**k!botinfo** - Ira mostrar as informaçoes de min. \n**!avatar** @usuário - Ira mostrar o avatar de um usuário.`)
+\n**${prefix}ajuda** - Ira te mandar a lista de comandos.
+**${prefix}botinfo** - Ira mostrar as informaçoes de min. 
+**${prefix}avatar** @usuário - Ira mostrar o avatar de um usuário.
+**${prefix}uptime**- Ira mostrar o tempo que o bot esta online.`)
       .setThumbnail("https://i.imgur.com/lEI7Gm6.gif")
       .setColor("#00f782")
       .setFooter("© Kallyᴮᴱᵀᴬ Outros")
