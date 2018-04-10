8d6640471b9dc98c66df989df13a34883599cb04
LockDzn <ryanssouza00@gmail.com>
2018-04-10 20:23:24 -0300

    :inbox_tray: | Novo comando k!limpar

--- comandos/limpar.js
+++ comandos/limpar.js
@@ -4,7 +4,7 @@ module.exports.run = async (client, message, args) =>{
 
    message.delete().catch(O_o=>{});
    if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.reply("você não tem permissão! :x:");
-   if(!args < "2") return message.reply("! :x:");
+   if(args < "2") return message.reply("por favor, digite o número maior que 2 para deletar as mensagens. :x:");
    if(!args[0]) return message.reply("por favor, digite o número maior que 2 para deletar as mensagens.");
    message.channel.bulkDelete(args[0]).then(() => {
    message.channel.send(`Chat limpo! Limpado **${args[0]}** mensagens por ${message.author}.`).then(msg => msg.delete(5000));
