b406a9ffb14aa9e1e9ee75511bef90072c25433a
LockDzn <ryanssouza00@gmail.com>
2018-04-11 17:03:48 -0300

    :arrows_counterclockwise: | Comando k!limpar atualizado

--- comandos/limpar.js
+++ comandos/limpar.js
@@ -8,18 +8,7 @@ module.exports.run = async (client, message, args) =>{
    if(args == "2") return message.reply("por favor, digite o número maior que 2 para deletar as mensagens. :x:");
    if(args == "1") return message.reply("por favor, digite o número maior que 2 para deletar as mensagens. :x:");
    if(!args[0]) return message.reply("por favor, digite o número maior que 2 para deletar as mensagens.");
-   message.channel.bulkDelete(args[0]).catch(error => 
-                                            const erro = new Discord.RichEmbed()
-                                             .setColor("ff0000")
-                                             .setAuthor('Deu um erro', client.user.avatarURL)
-         
-                                             .setDescription("```js\n" + error +"```")
-      
-      
-                                              .setTimestamp()
-                                              .setFooter("© Kallyᴮᴱᵀᴬ ERRO", message.author.avatarURL)
-                                             
-                                             message.channel.send(erro));
+   message.channel.bulkDelete(args[0]).catch(error => message.reply("**ERRO: Provavelmente as mensagens que você quer deletar ja passaram de 14 dias, sinto muito mais não posso deletar elas.**"));
    
    message.channel.send(`Chat limpo! Limpado **${args[0]}** mensagens por ${message.author}.`).then(msg => msg.delete(5000));
    
