fe4c9f5a25addb55f03efd0bcd944f151c09905f
LockDzn <ryanssouza00@gmail.com>
2018-04-11 16:52:38 -0300

    :arrows_counterclockwise: | Comando k!limpar atualizado

--- comandos/limpar.js
+++ comandos/limpar.js
@@ -3,17 +3,7 @@ const Discord = require("discord.js");
 module.exports.run = async (client, message, args) =>{
 
    message.delete().catch(O_o=>{});
-   const error = "None"
-   const erro = new Discord.RichEmbed()
-         .setColor("ff0000")
-         .setAuthor('Deu um erro', client.user.avatarURL)
-         
-         .setDescription("```js\n" + error +"```")
-      
-      
-         .setTimestamp()
-         .setFooter("© Kallyᴮᴱᵀᴬ ERRO", message.author.avatarURL)
-   
+
    if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.reply("você não tem permissão! :x:");
    if(args == "2") return message.reply("por favor, digite o número maior que 2 para deletar as mensagens. :x:");
    if(args == "1") return message.reply("por favor, digite o número maior que 2 para deletar as mensagens. :x:");
