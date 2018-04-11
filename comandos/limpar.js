2847d9dff6cf036674d4e5ec922e61da77b40f08
LockDzn <ryanssouza00@gmail.com>
2018-04-11 16:52:05 -0300

    :arrows_counterclockwise: | Comando k!limpar atualizado

--- comandos/limpar.js
+++ comandos/limpar.js
@@ -18,8 +18,19 @@ module.exports.run = async (client, message, args) =>{
    if(args == "2") return message.reply("por favor, digite o número maior que 2 para deletar as mensagens. :x:");
    if(args == "1") return message.reply("por favor, digite o número maior que 2 para deletar as mensagens. :x:");
    if(!args[0]) return message.reply("por favor, digite o número maior que 2 para deletar as mensagens.");
-   message.channel.bulkDelete(args[0]).catch(error => message.reply(`Não posso deletar as mensagens por: ${error} (**Provavelmente as mensagens**)`));
+   message.channel.bulkDelete(args[0]).catch(error => 
+                                            const erro = new Discord.RichEmbed()
+                                             .setColor("ff0000")
+                                             .setAuthor('Deu um erro', client.user.avatarURL)
+         
+                                             .setDescription("```js\n" + error +"```")
+      
+      
+                                              .setTimestamp()
+                                              .setFooter("© Kallyᴮᴱᵀᴬ ERRO", message.author.avatarURL)
+                                             
+                                             message.channel.send(erro));
+   
    message.channel.send(`Chat limpo! Limpado **${args[0]}** mensagens por ${message.author}.`).then(msg => msg.delete(5000));
-});
    
 }
