29916cadc6015fc7ed034627ac5e8406f3d4755a
LockDzn <ryanssouza00@gmail.com>
2018-04-11 15:51:07 -0300

    :arrows_counterclockwise: | Comando k!anuncio atualizado

--- comandos/anuncio.js
+++ comandos/anuncio.js
@@ -5,9 +5,18 @@ module.exports.run = async (client, message, args) =>{
    message.delete().catch(O_o=>{});
 
    if (message.member.hasPermission('MANAGE_GUILD')) {
+   const comousar = new Discord.RichEmbed()
+      .setAuthor("Kally", client.user.avatarURL)
+      .setTitle("k!kick")
+      .setDescription(`Ira kickar o usuário mencionado.`)
+      .setColor("#60d1f6")
+      .setFooter("© Kally - kally.glitch.me")
+      .addField("Como usar", "`k!kick @usuário <motivo>`")
+      .addField("Permissão", "O staff que for mutar tem que esta em um cargo com a permissão `Expulsar membros`")   
+      
    let mensg = args.join(" ");
    if(!mensg)
-      return message.reply("Por favor, digite a mensagem do anuncio!");
+      return message.channel.send(comousar)
       
    const anuncio = new Discord.RichEmbed()
       .setColor("0cff00")
