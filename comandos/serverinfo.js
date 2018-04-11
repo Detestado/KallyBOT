96e592140fafdc753d500bcba1455efe2035bbe4
LockDzn <ryanssouza00@gmail.com>
2018-04-11 14:20:11 -0300

    :inbox_tray: | Novo comando k!serverinfo

---
+++ comandos/serverinfo.js
@@ -0,0 +1,22 @@
+const Discord = require("discord.js");
+
+module.exports.run = async (client, message, args) =>{
+
+  const serverinfo = new Discord.RichEmbed()
+    .setAuthor(message.guild.name)
+    .setColor(3447003)
+    .setDescription(`Informações do servidor **${message.guild.name}**`)
+    
+    .addField('Dono', message.guild.owner.user.tag, true)
+    .addField('Quantidade de membros', `${message.guild.memberCount - message.guild.members.filter(m=>m.user.bot).size} (${message.guild.members.filter(m=>m.user.bot).size} bots)`)
+    .addField('Localização', message.guild.region)
+    .addField('Criado em', message.guild.createdAt.toLocaleString(), true)
+    
+    .addBlankField(true)
+    
+    .setThumbnail(message.guild.iconURL)
+     
+    .setTimestamp()
+    .setFooter(client.user.username, client.user.avatarURL);
+    
+ }
