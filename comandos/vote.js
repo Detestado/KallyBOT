76b9980aca2b5cb62ed1314dfa3e4305aaab4779
LockDzn <ryanssouza00@gmail.com>
2018-04-11 15:35:42 -0300

    :inbox_tray: | Novo comando k!vote

--- comandos/vote.js
+++ comandos/vote.js
@@ -3,14 +3,28 @@ const Discord = require("discord.js");
 module.exports.run = async (client, message, args) =>{
 
    message.delete().catch(O_o=>{});
+   
+    if(!message.member.hasPermission('MANAGE_GUILD'))
+      return message.reply("você não tem permissão! :x:");
+    
+    const comousar = new Discord.RichEmbed()
+      .setAuthor("Kally", client.user.avatarURL)
+      .setTitle("k!vote")
+      .setDescription(`Ira abrir uma votação no canal que a mensagem foi executada.`)
+      .setColor("#60d1f6")
+      .setFooter("© Kally - kally.glitch.me")
+      .addField("Como usar", "`k!vote <mensagem da votação>`")
+      .addField("Permissão", "O staff que for mutar tem que esta em um cargo com a permissão `Gerenciar servidor`")
+   
    let mensg = args.join(" ");
    if(!mensg)
-      return message.reply("por favor, digite a mensagem da votação!");
+      return message.channel.send(comousar);
+   
    const vote = new Discord.RichEmbed()
       .setAuthor("Votação")
       .setDescription("\n" + mensg)
       .setColor("#60d1f6")
-      .setFooter("© Kally Votação")
+      .setFooter(`Por: ${message.author.tag} - © Kally Votação`)
    
    message.channel.send("@everyone")
    const m = await message.channel.send(vote)
