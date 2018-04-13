ca4c75e5db57ac926561ef137318ee48a38c5f54
LockDzn <ryanssouza00@gmail.com>
2018-04-13 19:08:07 -0300

    :inbox_tray: | Novo comando k!userinfo

--- comandos/userinfo.js
+++ comandos/userinfo.js
@@ -27,7 +27,7 @@ module.exports.run = async (client, message, args) =>{
       .addField(":computer: Tag do discord", message.author.tag)   
       .addField(":bookmark_tabs: ID do discord", message.author.id) 
       .addField(":calendar_spiral: Criou conta em", message.author.createdAt.toLocaleString())
-      .addField(":date: Entro em", message.author.joinedAt.toLocaleString())
+      .addField(":date: Entro em", message.author.joinedAt)
       .setThumbnail(member.user.avatarURL)
       .setColor("#00a4ce")
       .setFooter("© Kallyᴮᴱᵀᴬ")
