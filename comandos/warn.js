969a4f603eaa061e41f961172c9c768275308241
LockDzn <ryanssouza00@gmail.com>
2018-05-02 16:14:20 -0300

    :inbox_tray: | Novo comando k!warn

--- comandos/warn.js
+++ comandos/warn.js
@@ -3,7 +3,7 @@ const Discord = require("discord.js");
 module.exports.run = async (client, message, args) =>{
 
     message.delete().catch(O_o=>{});
-    if(!message.member.hasPermission('KICK_MEMBERS'))
+    if(!message.member.hasPermission('MANAGE_MESSAGES'))
       return message.reply("você não tem permissão! :x:");
     
     const comousarkick = new Discord.RichEmbed()
