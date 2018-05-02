fe714a2c4dc717f6b6f0cd38ad90f5461fd3d8e1
LockDzn <ryanssouza00@gmail.com>
2018-05-02 19:10:14 -0300

    :inbox_tray: | Novo comando k!setgame

--- comandos/setgame.js
+++ comandos/setgame.js
@@ -4,15 +4,18 @@ module.exports.run = async (client, message, args) =>{
 
    if(message.member.id == "244537374258888725") {
    
-      let mensg = args.join(" ");
-      if(!mensg)
-        return message.reply("Por favor, digite o que o bot vai trasmitir!");
-        
-       
-      client.user.setPresence({ game: { name: mensg, type: 0 } });
-      message.reply('o bot esta jogando ' + mensg)
-      console.log('O bot esta jogando ' + mensg)
+      let type = args.slice(0)
+      if(!type)
+        return message.reply("Por favor, digite o tipo!");
+      let msg = args.slice(1).join(' ');
+      if(!msg)
+        return message.reply("Por favor, digite a mensagem!");
+      
+      if(type === "-J"){
+         client.user.setPresence({ game: { name: msg, type: 0 } });
+         message.reply('o bot esta jogando ' + msg)
 
+      }
+      
    }
-   
 }
