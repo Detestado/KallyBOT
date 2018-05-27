7b28a7694535465624eeecfd25249de3d2b2ab69
LockDzn <ryanssouza00@gmail.com>
2018-05-27 17:15:17 -0300

    :inbox_tray: | Novo comando k!userinfo

---
+++ comandos/userinfo.js
@@ -0,0 +1,42 @@
+const Discord = require("discord.js");
+const dateFormat = require('dateformat');
+
+const now = new Date();
+dateFormat(now, 'shortDate');
+
+module.exports.run = async (client, message, args) =>{
+
+    let member = message.mentions.members.first();
+   
+   if(member) {
+     
+   const info = new Discord.RichEmbed()
+      
+      .setTitle(":bust_in_silhouette: Informações de" + `${member.user.username}`)
+      .setThumbnail(member.user.avatarURL)
+      .addField(':computer: ID:', member.user.id)
+      .addField(":video_game: Jogando:", `${member.user.presence.game ? `${member.user.presence.game.name}` : "Não está jogando."}`)
+      .addField(':inbox_tray: Entrou nesse servido em:', dateFormat(member.joinedAt, 'mm/dd/yyyy'))
+      .addField(':inbox_tray: Conta criado em:', dateFormat(member.user.createdAt, 'mm/dd/yyyy'))
+      .setColor("#00a4ce")
+      .setFooter("© Kally")
+   
+      message.channel.send(info)
+   
+   } else {
+      
+    const info = new Discord.RichEmbed()
+      
+    .setTitle(":bust_in_silhouette: Informações de" + ` ${message.author.tag}`)
+    .setThumbnail(message.author.avatarURL)
+    .addField(':computer: ID:', message.author.id)
+    .addField(":video_game: Jogando:", `${message.author.presence.game ? `${message.author.presence.game.name}` : "Não está jogando."}`)
+    .addField(':inbox_tray: Entrou nesse servido em:', dateFormat(message.author.joinedAt, 'mm/dd/yyyy'))
+    .addField(':inbox_tray: Conta criado em:', dateFormat(message.author.createdAt, 'mm/dd/yyyy'))
+    .setColor("#00a4ce")
+    .setFooter("© Kally")
+ 
+    message.channel.send(info)
+      
+   }
+}
