2b8e0878cdb2eb3a3a9a0e818816edec9f11fc65
LockDzn <ryanssouza00@gmail.com>
2018-05-27 21:21:28 -0300

    :inbox_tray: | Novo comando k!userinfo

--- comandos/userinfo.js
+++ comandos/userinfo.js
@@ -12,12 +12,12 @@ module.exports.run = async (client, message, args) =>{
      
    const info = new Discord.RichEmbed()
       
-      .setTitle(":bust_in_silhouette: Informações de" + `${member.user.username}`)
+      .setTitle(":bust_in_silhouette: Informações de" + ` ${member.user.username}`)
       .setThumbnail(member.user.avatarURL)
       .addField(':computer: ID:', member.user.id)
       .addField(":video_game: Jogando:", `${member.user.presence.game ? `${member.user.presence.game.name}` : "Não está jogando."}`)
-      .addField(':inbox_tray: Entrou nesse servido em:', dateFormat(member.joinedAt, 'mm/dd/yyyy'))
-      .addField(':inbox_tray: Conta criado em:', dateFormat(member.user.createdAt, 'mm/dd/yyyy'))
+      .addField(':inbox_tray: Entrou nesse servido em:', dateFormat(member.joinedAt, 'dd/mm/yyyy'))
+      .addField(':inbox_tray: Conta criado em:', dateFormat(member.user.createdAt, 'dd/mm/yyyy'))
       .setColor("#00a4ce")
       .setFooter("© Kally")
    
@@ -31,8 +31,8 @@ module.exports.run = async (client, message, args) =>{
     .setThumbnail(message.author.avatarURL)
     .addField(':computer: ID:', message.author.id)
     .addField(":video_game: Jogando:", `${message.author.presence.game ? `${message.author.presence.game.name}` : "Não está jogando."}`)
-    .addField(':inbox_tray: Entrou nesse servido em:', dateFormat(message.author.joinedAt, 'mm/dd/yyyy'))
-    .addField(':inbox_tray: Conta criado em:', dateFormat(message.author.createdAt, 'mm/dd/yyyy'))
+    .addField(':inbox_tray: Entrou nesse servido em:', dateFormat(message.author.joinedAt, 'dd/mm/yyyy'))
+    .addField(':inbox_tray: Conta criado em:', dateFormat(message.author.createdAt, 'dd/mm/yyyy'))
     .setColor("#00a4ce")
     .setFooter("© Kally")
  
