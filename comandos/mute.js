aabb47859647d9669ee0272ae0ae15c45149641f
LockDzn <ryanssouza00@gmail.com>
2018-06-02 23:34:32 -0300

    :arrows_counterclockwise: | Comando k!mute atualizado

--- comandos/mute.js
+++ comandos/mute.js
@@ -3,23 +3,6 @@ const Discord = require("discord.js");
 module.exports.run = async (client, message, args) =>{
 
    message.delete().catch(O_o=>{});
-   if (message.member.hasPermission('MANAGE_MESSAGES'))
-         if (!message.guild.roles.find("name", "Mutado")) {
-             const norole = new Discord.RichEmbed()
-                .setColor("ff0000")
-                .setAuthor('Deu um erro', client.user.avatarURL)
-
-                .setDescription(`${message.author}, o cargo **Mutado** não foi encontrado. :slight_frown: 
-Crie um cargo com o nome "**Mutado**", assim poderei mutar o usuário!`)
-
-
-                .setTimestamp()
-                .setFooter("© Kallyᴮᴱᵀᴬ ERRO", message.author.avatarURL)
-      
-            message.channel.send(norole)
-      
-      
-    } else {
    const comousar = new Discord.RichEmbed()
       .setAuthor("Kally", client.user.avatarURL)
       .setTitle("k!mute")
