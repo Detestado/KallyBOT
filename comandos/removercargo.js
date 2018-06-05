47f61f875516a17ffffcac69ff95646475d623ff
LockDzn <ryanssouza00@gmail.com>
2018-06-05 18:56:37 -0300

    :inbox_tray: | Novo comando k!removercargo

--- comandos/removercargo.js
+++ comandos/removercargo.js
@@ -2,31 +2,83 @@ const Discord = require("discord.js");
 
 module.exports.run = async (client, message, args) =>{
 
-   if(message.author.id === "244537374258888725"){
-    message.delete().catch(O_o=>{});
-    let servidornome = args.join(" ");
-    if(!servidornome)
-        return message.reply("por favor, digite o nome do servidor!")
-
-    let guild = client.guilds.find("name", servidornome);
-    if(!guild)
-        return message.reply("por favor, o servidor que você digitou não existe!")
-
-    guild.leave()
-    message.reply(`você me removeu do servidor ${servidornome}! :white_check_mark:`)
-
-    const sai = new Discord.RichEmbed()
-        .setAuthor(`${guild.name} | Removido`)
-        .setDescription(`Fui removido do servidor **${guild.name}** (ID: ${guild.id})!`)
-        .setColor("#ff0000")
-        .addField("Membros", `Com **${guild.memberCount}** membros`)
-        .addField("Dono", `${guild.owner} (ID: ${guild.owner.id})`)
-
-        .setFooter(`${message.author.tag} me removeu do servidor!`, guild.iconURL)
-
-    client.channels.get("429844744110211072").send(sai);
-
-
+   message.delete().catch(O_o=>{});
+   if (message.member.hasPermission('MANAGE_ROLES_OR_PERMISSIONS')) {
+    const comousar = new Discord.RichEmbed()
+       .setAuthor("Kally", client.user.avatarURL)
+       .setTitle("k!setcargo")
+       .setDescription(`Ira remover o cargo do usuário mencionado.`)
+       .setColor("#60d1f6")
+       .setFooter("© Kally - kally.glitch.me")
+       .addField("Como usar", "`k!removercargo @usuário <nome do cargo>`")
+       .addField("Permissão", "O staff que for remover o cargo tem que está em um cargo com a permissão `Gerenciar cargos`")
+       .addField("Info", "A **minha** permissão tem que está **acima dos cargos** que eu vou remover!")
+   let member = message.mentions.members.first();
+   if(!member)
+    return message.channel.send(comousar).then(msg => msg.delete(10000));
+   let rolename = args.slice(1).join(' ');
+   if(!rolename)
+    return message.channel.send("por favor, digite o cargo para remover! (**OBS:** Digite sem o @ e se tiver emojis, digite com emojis! :)").then(msg => msg.delete(10000));
    
+    if (!message.guild.roles.find("name", rolename)) {
+      
+        const norole = new Discord.RichEmbed()
+           .setColor("ff0000")
+           .setAuthor('Deu um erro', client.user.avatarURL)
+           
+           .setDescription(`O cargo **${rolename}** não existe (**OBS:** Coloque o nome do cargo certo, emojis, minúsculo e maiúsculo nos lugares certos **E SEM @**.)`)
+        
+        
+           .setTimestamp()
+           .setFooter("© Kally ERRO", message.author.avatarURL)
+        
+        message.channel.send(message.author, norole)
+        return;
+        
+       } else {
+          let role = message.guild.roles.find("name", rolename);
+          member.removeRole(role)
+          message.reply("O cargo `" +rolename+"`"+` foi removido de ${member}!`).then(msg => msg.delete(5000));
+        
+    }
+   if(message.guild.channels.find("name", "changelog")){
+      let guild = message.guild.channels.find("name", "changelog");
+      const changelog = new Discord.RichEmbed()
+        .setTitle("🔧 Changelog")
+        .setDescription(member+ " foi removido de `" +rolename+"`!")
+        .setColor("#3f55fc")
+        .setFooter("© Kally Changelog")  
+      guild.send(changelog) 
+   }
+   if(message.guild.channels.find("name", "change-log")){
+    let guild = message.guild.channels.find("name", "change-log");
+    const changelog = new Discord.RichEmbed()
+      .setTitle("🔧 Change-log")
+      .setDescription(member+ " foi removido de `" +rolename+"`!")
+      .setColor("#3f55fc")
+      .setFooter("© Kally Change-log")  
+    guild.send(changelog) 
+   }
+   if(message.guild.channels.find("name", "🔧change-log")){
+    let guild = message.guild.channels.find("name", "🔧change-log");
+    const changelog = new Discord.RichEmbed()
+      .setTitle(":wrench: Change-log")
+      .setDescription(member+ " foi removido de `" +rolename+"`!")
+      .setColor("#3f55fc")
+      .setFooter("© Kally 🔧 Change-log")  
+    guild.send(changelog) 
+   }
+   if(message.guild.channels.find("name", "🔧changelog")){
+    let guild = message.guild.channels.find("name", "🔧changelog");
+    const changelog = new Discord.RichEmbed()
+      .setTitle(":wrench: Change-log")
+      .setDescription(member+ " foi removido de `" +rolename+"`!")
+      .setColor("#3f55fc")
+      .setFooter("© Kally 🔧 Changelog")  
+    guild.send(changelog) 
+   }
+   }else {
+        const emojinop = client.guilds.get("420316735149965322").emojis.find("name", "KallyNop");
+        message.reply("você não tem permissão! " + emojinop).then(msg => msg.delete(5000));
    }
 }
