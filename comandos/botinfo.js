dbf0904d4407e878c5092fa383fd994e636e7542
LockDzn <ryanssouza00@gmail.com>
2018-06-06 12:55:57 -0300

    :arrows_counterclockwise: | Comando k!botinfo atualizado

--- comandos/botinfo.js
+++ comandos/botinfo.js
@@ -12,19 +12,22 @@ module.exports.run = async (client, message, args) =>{
    const h = uptime += date.getUTCHours() + 'h, ';
    const m = uptime += date.getUTCMinutes() + 'm, ';
    const s = uptime += date.getUTCSeconds() + 's';
-   
+
+   const emojijs = client.guilds.get("420316735149965322").emojis.find("name", "javascript");
+   const emojinode = client.guilds.get("420316735149965322").emojis.find("name", "nodejs");
+   const emojipython = client.guilds.get("420316735149965322").emojis.find("name", "python");
    const info = new Discord.RichEmbed()
       .setColor("#0092ca")
       .setAuthor(client.user.username, client.user.avatarURL)
       .setThumbnail(client.user.avatarURL)
 
    
-      .setDescription(`Olá, me chamo Kally, sou focado na moderação dos servidores do discord, entre no meu servidor [aqui](https://discord.gg/nbEqSrv), 
-lá o meu dono pode te ajuda e você vai receber atualizações e também entre no meu [site](https://kally.glitch.me/)!\n
-Estou em **${client.guilds.size}** servidores do discord. \nSou feito em **JS** (Node.js/JavaScript) e em **Python** :robot: 
+      .setDescription(`Olá, me chamo Kally, sou focado na moderação dos servidores do discord.\n\nEstou em **${client.guilds.size} servidores** do discord. \nFui feito em ${emojijs} **JavaScript** usando ${emojinode} **Node.js** e em ${emojipython} **Python** :robot: 
 \nFazem **` + s + `** que eu estou acordado (ou seja, meu uptime atual) :zzz:\n \nUse **k!ajuda** para saber meus comandos!`)
    
-      .addField(":heart: Melhores pessoas:", ` **LockDzn#8368** Foi ele que me criou! :grin:\n**${message.author.username}#${message.author.discriminator}** Por estar falando comigo! :smile: `)
+      .addField(":question: Suporte", `https://discord.gg/fsSNJJH`)
+      .addField(":computer: Site", `https://kally.glitch.me`)
+      .addField(":heart: Melhores pessoas:", `**${dono}** Foi ele que me criou! :grin:\n**${message.author}** Por estar falando comigo! :smile: `)
       .setFooter("Kally foi criado por LockDzn - Site: bit.ly/LockDzn", dono.user.avatarURL)
    
    message.channel.send(info)
